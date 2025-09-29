b7e1f2c7e0525da2d35a1b06e12b370a6eecdeda
Abdulaziz <abdulaziz@Abdulazizs-MacBook-Air.local>
2025-09-30 00:45:41 +0300

    ,

--- edujeel/lib/style/app_colors.dart
+++ edujeel/lib/style/app_colors.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const white = Color.fromARGB(255, 255, 255, 255);
+  static const primary = Color(0xff0582CA);
+  static const secondary = Color(0xffF2AC33);
+  static const error = Color(0xffD7263D);
+  static const green = Color(0xff17BC6D);
+  static const grey = Color(0xff404040);
+  static const lightGrey = Color(0xff8C8C8C);
+  static const veryLightGrey = Color(0xffD9D9D9);
+  static const white = Color(0xffFFFFFF);
 }
